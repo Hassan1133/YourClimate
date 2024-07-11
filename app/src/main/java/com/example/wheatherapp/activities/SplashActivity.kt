c2250ab347ab0e3c25e125872928c1db1e654f47
Hassan1133 <mhassanidrees575@gmail.com>
2024-07-11 23:00:33 +0500

    Splash Screen Desgin Commit

---
+++ app/src/main/java/com/example/wheatherapp/activities/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.wheatherapp.activities
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wheatherapp.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
